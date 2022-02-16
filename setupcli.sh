945c1fd363d3643b9861c884f8eaf66f1d88890b
gowriand <95476026+gowriand@users.noreply.github.com>
2022-02-16 22:29:45 +0530

    Update setupcli.sh

--- setupcli.sh
+++ setupcli.sh
@@ -2,7 +2,7 @@
 # https://linoxide.com/how-to-install-aws-cli-on-ubuntu-20-04/
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt-get update
-sudo apt install unzip
+sudo apt install -y unzip
 sudo unzip awscliv2.zip
 sudo ./aws/install
 aws --version
