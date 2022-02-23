7af27d3467399cae3472f0cba4f105a8940c58ef
gowriand <95476026+gowriand@users.noreply.github.com>
2022-02-23 11:05:03 +0530

    Update setup-awscli.sh

--- setupscripts/setup-awscli.sh
+++ setupscripts/setup-awscli.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # https://linoxide.com/how-to-install-aws-cli-on-ubuntu-20-04/
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+
 sudo apt-get update
+
+# First install boto
+sudo apt-get install -y pip
+sudo pip install boto3
+
+
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
 sudo apt install -y unzip
 sudo unzip /tmp/awscliv2.zip -d /tmp/
 sudo /tmp/aws/install
